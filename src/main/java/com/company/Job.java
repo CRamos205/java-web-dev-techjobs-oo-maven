2e6f3f20a070f48f00ccef03093c7e51de77c04d
cramos205 <cramos205@cable.comcast.com>
2021-07-08 11:06:36 -0400

    new

--- src/main/java/com/company/Job.java
+++ src/main/java/com/company/Job.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Objects;
+
 public class Job {
     private static int nextId = 1;
 
@@ -10,6 +12,47 @@ public class Job {
     private PositionType positionType;
     private CoreCompetency coreCompetency;
 
+    public Job() {
+        this.id = nextId;
+        nextId++;
+    }
+    public Job(String name, Employer employer, Location location, PositionType positionType, CoreCompetency coreCompetency){
+        this.name = name;
+        this.employer = employer;
+        this.location = location;
+        this.positionType = positionType;
+        this.coreCompetency = coreCompetency;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, employer, location, positionType, coreCompetency);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public PositionType getPositionType() {
+        return positionType;
+    }
+
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
     // TODO: add two constructors - one to initialize a unique ID and a second to initialize the other five fields. The second constructor should also call the first in order to initialize the 'id' field
 
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields match.
